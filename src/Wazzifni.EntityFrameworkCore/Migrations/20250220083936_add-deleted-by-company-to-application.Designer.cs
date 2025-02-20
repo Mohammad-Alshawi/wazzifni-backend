3a078de9d4d6a9d4eed94829434d479dbbc06208
Mohammad A-sh <mohammad.a.alshawi@gmail.com>
2025-02-20 11:46:42 +0300

    apply migration

--- src/Wazzifni.EntityFrameworkCore/Migrations/20250220083936_add-deleted-by-company-to-application.Designer.cs
+++ src/Wazzifni.EntityFrameworkCore/Migrations/20250220083936_add-deleted-by-company-to-application.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Wazzifni.EntityFrameworkCore;
 
@@ -11,9 +12,11 @@ using Wazzifni.EntityFrameworkCore;
 namespace Wazzifni.Migrations
 {
     [DbContext(typeof(WazzifniDbContext))]
-    partial class WazzifniDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250220083936_add-deleted-by-company-to-application")]
+    partial class adddeletedbycompanytoapplication
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Wazzifni.EntityFrameworkCore/Migrations/20250220083936_add-deleted-by-company-to-application.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wazzifni.Migrations
+{
+    /// <inheritdoc />
+    public partial class adddeletedbycompanytoapplication : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DeletedByCompany",
+                table: "WorkApplications",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedByCompany",
+                table: "WorkApplications");
+        }
+    }
+}
