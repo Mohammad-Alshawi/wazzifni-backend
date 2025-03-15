dd94019ffe324db1a67c81a490c4ce24bea01627
Mohammad A-sh <mohammad.a.alshawi@gmail.com>
2025-03-15 14:38:53 +0300

    up sixe attachment

--- src/Wazzifni.Core/Configuration/AppSettingProvider.cs
+++ src/Wazzifni.Core/Configuration/AppSettingProvider.cs
@@ -10,7 +10,7 @@ namespace Wazzifni.Configuration
             return new[]
             {
                 new SettingDefinition(AppSettingNames.UiTheme, "red", scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User, clientVisibilityProvider: new VisibleSettingClientVisibilityProvider()),
-                new SettingDefinition(AppSettingNames.FileSize, "5", scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User, clientVisibilityProvider: new VisibleSettingClientVisibilityProvider()),
+                new SettingDefinition(AppSettingNames.FileSize, "25", scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User, clientVisibilityProvider: new VisibleSettingClientVisibilityProvider()),
                 new SettingDefinition(AppSettingNames.ImageSize, "100", scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User, clientVisibilityProvider: new VisibleSettingClientVisibilityProvider())
             };
         }
