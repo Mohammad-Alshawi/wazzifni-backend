8ae7906367cb36b21fff8ee55aaa4cde91729287
Mohammad-Alshawi <mohammad.a.alshawi@gmail.com>
2025-05-28 15:09:31 +0300

    apply migration

---
+++ src/Wazzifni.EntityFrameworkCore/Migrations/20250528120557_edit-NumberOfRegisteredPeople.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wazzifni.Migrations
+{
+    /// <inheritdoc />
+    public partial class editNumberOfRegisteredPeople : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfRegisteredPeople",
+                table: "CourseRegistrationRequests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberOfRegisteredPeople",
+                table: "CourseRegistrationRequests");
+        }
+    }
+}
